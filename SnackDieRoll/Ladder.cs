4e7414910bafb164e04e8127ea160c5f55e1897b
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-08 23:45:48 +0530

    [TOMESHWAR]ADD:- SNACK LADDER SINGLE PLAYER START POSTION 0

---
+++ SnackDieRoll/Ladder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackDieRoll
+{
+    public class Ladder
+    {
+
+        const int position = 0;
+        public int dieRoll()
+        {
+            return position;
+        }
+       
+       
+    }
+}
---
+++ SnackDieRoll/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SnackDieRoll;
+
+public class Program
+{
+
+    public static void Main(String[] args)
+    {
+        Ladder Game = new Ladder();
+        int Start = Game.dieRoll();
+        Console.WriteLine("Welcome in the Snack Game, Your Position is" + " " + Start);
+    }
+
+}
